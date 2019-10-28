0d3496f0c79eb654ad76b658f5f7ff23248808c5
ayush151996 <ayured2011@gmail.com>
2019-10-28 13:33:04 +0530

    Material UI added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DetailComponent } from  '../detail/detail.component'
-
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-data=[];
+export class DashboardComponent  implements OnInit {
+data: any;
+dataS:any;
   constructor(public dialog: MatDialog) { }
-
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  ngOnInit() {
+    this.data = JSON.parse(localStorage.getItem('prdcts'));
+    this.dataS = new MatTableDataSource(this.data);
+    //this.dataS = this.data;
+    console.log(this.dataS);
+    this.dataS.paginator = this.paginator;
+  }
   
-
   detailing(row){
     
     const dialogRef = this.dialog.open(DetailComponent, {
@@ -21,10 +29,7 @@ data=[];
       data: row,
     });
   }
-  ngOnInit() {
-    this.data = JSON.parse(localStorage.getItem('prdcts'))
-    console.log(this.data)
-  }
+  
   displayedColumns: string[] = ['fname', 'prc'];
   //dataSource = this.data;
 }
